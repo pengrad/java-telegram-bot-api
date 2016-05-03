312d2edeae447ddaaf49b2c95dcc6786ee6e523e
Stas Parshin <s.a.parshin@gmail.com>
2016-05-04 00:04:52 +0700

    SendContact

---
+++ src/main/java/com/pengrad/telegrambot/request/SendContact.java
@@ -0,0 +1,18 @@
+package com.pengrad.telegrambot.request;
+
+/**
+ * stas
+ * 5/1/16.
+ */
+public class SendContact extends AbstractSendRequest<SendContact> {
+
+    public SendContact(Object chatId, String phoneNumber, String firstName) {
+        super(chatId);
+        add("phone_number", phoneNumber);
+        add("first_name", firstName);
+    }
+
+    public SendContact lastName(String lastName) {
+        return add("last_name", lastName);
+    }
+}
