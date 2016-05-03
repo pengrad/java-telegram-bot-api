05b8a34cfc7b14d4ecd13ead44bd618b5f6e1cc3
Stas Parshin <s.a.parshin@gmail.com>
2016-05-03 23:59:35 +0700

    SendVenue

---
+++ src/main/java/com/pengrad/telegrambot/request/SendVenue.java
@@ -0,0 +1,20 @@
+package com.pengrad.telegrambot.request;
+
+/**
+ * stas
+ * 5/1/16.
+ */
+public class SendVenue extends AbstractSendRequest<SendVenue> {
+
+    public SendVenue(Object chatId, float latitude, float longitude, String title, String address) {
+        super(chatId);
+        add("latitude", latitude);
+        add("longitude", longitude);
+        add("title", title);
+        add("address", address);
+    }
+
+    public SendVenue foursquareId(String foursquareId) {
+        return add("foursquare_id", foursquareId);
+    }
+}
