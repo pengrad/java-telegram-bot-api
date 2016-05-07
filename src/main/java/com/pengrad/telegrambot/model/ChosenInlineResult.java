61d4a69c8335c4f122c453e7ead97b5a4083961d
Stas Parshin <s.a.parshin@gmail.com>
2016-05-07 20:33:00 +0700

    Add location and inline_message_id to the ChosenInlineResult

--- src/main/java/com/pengrad/telegrambot/model/ChosenInlineResult.java
+++ src/main/java/com/pengrad/telegrambot/model/ChosenInlineResult.java
@@ -8,6 +8,8 @@ public class ChosenInlineResult {
 
     private String result_id;
     private User from;
+    private Location location;
+    private String inline_message_id;
     private String query;
 
     ChosenInlineResult() {
@@ -19,6 +21,14 @@ public class ChosenInlineResult {
 
     public User from() {
         return from;
+    }
+
+    public Location location() {
+        return location;
+    }
+
+    public String inlineMessageId() {
+        return inline_message_id;
     }
 
     public String query() {
@@ -34,6 +44,9 @@ public class ChosenInlineResult {
 
         if (result_id != null ? !result_id.equals(that.result_id) : that.result_id != null) return false;
         if (from != null ? !from.equals(that.from) : that.from != null) return false;
+        if (location != null ? !location.equals(that.location) : that.location != null) return false;
+        if (inline_message_id != null ? !inline_message_id.equals(that.inline_message_id) : that.inline_message_id != null)
+            return false;
         return query != null ? query.equals(that.query) : that.query == null;
 
     }
@@ -48,6 +61,8 @@ public class ChosenInlineResult {
         return "ChosenInlineResult{" +
                 "result_id='" + result_id + '\'' +
                 ", from=" + from +
+                ", location=" + location +
+                ", inline_message_id='" + inline_message_id + '\'' +
                 ", query='" + query + '\'' +
                 '}';
     }
