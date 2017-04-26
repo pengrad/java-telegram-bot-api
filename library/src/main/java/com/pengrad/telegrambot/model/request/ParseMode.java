1c95f24c7d4d2b5778ed4a1e85663185f401267d
IngvarJackal <rz703a@gmail.com>
2017-04-26 21:31:43 +0300

    -serialization for enums

--- library/src/main/java/com/pengrad/telegrambot/model/request/ParseMode.java
+++ library/src/main/java/com/pengrad/telegrambot/model/request/ParseMode.java
@@ -1,12 +1,9 @@
 package com.pengrad.telegrambot.model.request;
 
-import java.io.Serializable;
-
 /**
  * stas
  * 10/21/15.
  */
-public enum ParseMode implements Serializable {
-    Markdown, HTML;
-    private final static long serialVersionUID = 0L;
+public enum ParseMode {
+    Markdown, HTML
 }
