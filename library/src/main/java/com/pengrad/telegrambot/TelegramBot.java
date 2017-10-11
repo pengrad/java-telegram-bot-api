1666aaca44f017d3305780d195634bfac00b8ce0
Stas Parshin <s.a.parshin@gmail.com>
2017-10-11 23:36:48 +0700

    TelegramBot.Builder instead of TelegramBotAdapter

--- library/src/main/java/com/pengrad/telegrambot/TelegramBot.java
+++ library/src/main/java/com/pengrad/telegrambot/TelegramBot.java
@@ -1,5 +1,6 @@
 package com.pengrad.telegrambot;
 
+import com.google.gson.Gson;
 import com.pengrad.telegrambot.impl.FileApi;
 import com.pengrad.telegrambot.impl.TelegramBotClient;
 import com.pengrad.telegrambot.impl.UpdatesHandler;
@@ -7,6 +8,9 @@ import com.pengrad.telegrambot.model.File;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.response.BaseResponse;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * Stas Parshin
@@ -18,10 +22,14 @@ public class TelegramBot {
     private final FileApi fileApi;
     private final UpdatesHandler updatesHandler;
 
-    TelegramBot(TelegramBotClient api, FileApi fileApi) {
-        this.api = api;
-        this.fileApi = fileApi;
-        this.updatesHandler = new UpdatesHandler();
+    public TelegramBot(String botToken) {
+        this(new Builder(botToken));
+    }
+
+    TelegramBot(Builder builder) {
+        this.api = builder.api;
+        this.fileApi = builder.fileApi;
+        this.updatesHandler = builder.updatesHandler;
     }
 
     public <T extends BaseRequest, R extends BaseResponse> R execute(BaseRequest<T, R> request) {
@@ -46,5 +54,67 @@ public class TelegramBot {
 
     public void removeGetUpdatesListener() {
         updatesHandler.stop();
+    }
+
+    public static final class Builder {
+
+        static final String API_URL = "https://api.telegram.org/bot";
+
+        private final String botToken;
+        private TelegramBotClient api;
+        private final FileApi fileApi;
+        private final UpdatesHandler updatesHandler;
+
+        private OkHttpClient okHttpClient;
+        private String apiUrl;
+
+        public Builder(String botToken) {
+            this.botToken = botToken;
+            api = new TelegramBotClient(client(null), gson(), apiUrl(API_URL, botToken));
+            fileApi = new FileApi(botToken);
+            updatesHandler = new UpdatesHandler();
+        }
+
+        public Builder debug() {
+            okHttpClient = client(httpLoggingInterceptor());
+            return this;
+        }
+
+        public Builder okHttpClient(OkHttpClient client) {
+            okHttpClient = client;
+            return this;
+        }
+
+        public Builder apiUrl(String apiUrl) {
+            this.apiUrl = apiUrl;
+            return this;
+        }
+
+        public TelegramBot build() {
+            if (okHttpClient != null || apiUrl != null) {
+                OkHttpClient client = okHttpClient != null ? okHttpClient : client(null);
+                String baseUrl = apiUrl(apiUrl != null ? apiUrl : API_URL, botToken);
+                api = new TelegramBotClient(client, gson(), baseUrl);
+            }
+            return new TelegramBot(this);
+        }
+
+        private static OkHttpClient client(Interceptor interceptor) {
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            if (interceptor != null) builder.addInterceptor(interceptor);
+            return builder.build();
+        }
+
+        private static Interceptor httpLoggingInterceptor() {
+            return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+        }
+
+        private static Gson gson() {
+            return new Gson();
+        }
+
+        private static String apiUrl(String apiUrl, String botToken) {
+            return apiUrl + botToken + "/";
+        }
     }
 }
--- library/src/main/java/com/pengrad/telegrambot/TelegramBotAdapter.java
+++ library/src/main/java/com/pengrad/telegrambot/TelegramBotAdapter.java
@@ -1,53 +1,33 @@
 package com.pengrad.telegrambot;
 
-import com.google.gson.Gson;
-import com.pengrad.telegrambot.impl.FileApi;
-import com.pengrad.telegrambot.impl.TelegramBotClient;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * stas
  * 8/4/15.
  */
+
+/**
+ * @deprecated Use TelegramBot(botToken) or TelegramBot.Builder
+ */
+@Deprecated
 public class TelegramBotAdapter {
 
     public static final String API_URL = "https://api.telegram.org/bot";
 
     public static TelegramBot build(String botToken) {
-        return buildCustom(botToken, client(null));
+        return new TelegramBot(botToken);
     }
 
     public static TelegramBot buildDebug(String botToken) {
-        return buildCustom(botToken, client(httpLoggingInterceptor()));
+        return new TelegramBot.Builder(botToken).debug().build();
     }
 
     public static TelegramBot buildCustom(String botToken, OkHttpClient okHttpClient) {
-        return buildCustom(botToken, okHttpClient, API_URL);
+        return new TelegramBot.Builder(botToken).okHttpClient(okHttpClient).build();
     }
 
     public static TelegramBot buildCustom(String botToken, OkHttpClient okHttpClient, String apiUrl) {
-        TelegramBotClient client = new TelegramBotClient(okHttpClient, gson(), apiUrl(apiUrl, botToken));
-        FileApi fileApi = new FileApi(botToken);
-        return new TelegramBot(client, fileApi);
-    }
-
-    private static OkHttpClient client(Interceptor interceptor) {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        if (interceptor != null) builder.addInterceptor(interceptor);
-        return builder.build();
-    }
-
-    private static Interceptor httpLoggingInterceptor() {
-        return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
-    }
-
-    private static Gson gson() {
-        return new Gson();
-    }
-
-    private static String apiUrl(String apiUrl, String botToken) {
-        return apiUrl + botToken + "/";
+        return new TelegramBot.Builder(botToken).okHttpClient(okHttpClient).apiUrl(apiUrl).build();
     }
 }
