dee9d7ee5018420a710b128e5d8f74118deaf89c
Stas Parshin <s.a.parshin@gmail.com>
2020-03-31 21:26:37 +0800

    Added the field thumb to the StickerSet object

--- library/src/main/java/com/pengrad/telegrambot/model/StickerSet.java
+++ library/src/main/java/com/pengrad/telegrambot/model/StickerSet.java
@@ -15,6 +15,7 @@ public class StickerSet implements Serializable {
     private Boolean is_animated;
     private Boolean contains_masks;
     private Sticker[] stickers;
+    private PhotoSize thumb;
 
     public String name() {
         return name;
@@ -36,6 +37,10 @@ public class StickerSet implements Serializable {
         return stickers;
     }
 
+    public PhotoSize thumb() {
+        return thumb;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -46,9 +51,13 @@ public class StickerSet implements Serializable {
         if (name != null ? !name.equals(that.name) : that.name != null) return false;
         if (title != null ? !title.equals(that.title) : that.title != null) return false;
         if (is_animated != null ? !is_animated.equals(that.is_animated) : that.is_animated != null) return false;
-        if (contains_masks != null ? !contains_masks.equals(that.contains_masks) : that.contains_masks != null) return false;
+        if (contains_masks != null ? !contains_masks.equals(that.contains_masks) : that.contains_masks != null)
+            return false;
         // Probably incorrect - comparing Object[] arrays with Arrays.equals
-        return Arrays.equals(stickers, that.stickers);
+        if (!Arrays.equals(stickers, that.stickers)) return false;
+        if (thumb != null ? !thumb.equals(that.thumb) : that.thumb != null) return false;
+
+        return true;
     }
 
     @Override
@@ -58,6 +67,7 @@ public class StickerSet implements Serializable {
         result = 31 * result + (is_animated != null ? is_animated.hashCode() : 0);
         result = 31 * result + (contains_masks != null ? contains_masks.hashCode() : 0);
         result = 31 * result + Arrays.hashCode(stickers);
+        result = 31 * result + (thumb != null ? thumb.hashCode() : 0);
         return result;
     }
 
@@ -69,6 +79,7 @@ public class StickerSet implements Serializable {
                 ", is_animated=" + is_animated +
                 ", contains_masks=" + contains_masks +
                 ", stickers=" + Arrays.toString(stickers) +
+                ", thumb=" + thumb +
                 '}';
     }
 }
