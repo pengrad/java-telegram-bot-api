6daed1db7cd356fb4a349205f2d75bb7ba97aeef
Stas Parshin <s.a.parshin@gmail.com>
2021-03-10 13:58:16 +0800

    Added updates about member status changes in chats, represented by the class ChatMemberUpdated and the fields my_chat_member and chat_member in the Update class.

--- library/src/main/java/com/pengrad/telegrambot/model/Update.java
+++ library/src/main/java/com/pengrad/telegrambot/model/Update.java
@@ -1,6 +1,7 @@
 package com.pengrad.telegrambot.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * stas
@@ -21,6 +22,8 @@ public class Update implements Serializable {
     private PreCheckoutQuery pre_checkout_query;
     private Poll poll;
     private PollAnswer poll_answer;
+    private ChatMemberUpdated my_chat_member;
+    private ChatMemberUpdated chat_member;
 
     public Integer updateId() {
         return update_id;
@@ -70,33 +73,33 @@ public class Update implements Serializable {
         return poll_answer;
     }
 
+    public ChatMemberUpdated myChatMember() {
+        return my_chat_member;
+    }
+
+    public ChatMemberUpdated chatMember() {
+        return chat_member;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Update update = (Update) o;
-
-        if (update_id != null ? !update_id.equals(update.update_id) : update.update_id != null) return false;
-        if (message != null ? !message.equals(update.message) : update.message != null) return false;
-        if (edited_message != null ? !edited_message.equals(update.edited_message) : update.edited_message != null)
-            return false;
-        if (channel_post != null ? !channel_post.equals(update.channel_post) : update.channel_post != null)
-            return false;
-        if (edited_channel_post != null ? !edited_channel_post.equals(update.edited_channel_post) : update.edited_channel_post != null)
-            return false;
-        if (inline_query != null ? !inline_query.equals(update.inline_query) : update.inline_query != null)
-            return false;
-        if (chosen_inline_result != null ? !chosen_inline_result.equals(update.chosen_inline_result) : update.chosen_inline_result != null)
-            return false;
-        if (callback_query != null ? !callback_query.equals(update.callback_query) : update.callback_query != null)
-            return false;
-        if (shipping_query != null ? !shipping_query.equals(update.shipping_query) : update.shipping_query != null)
-            return false;
-        if (pre_checkout_query != null ? !pre_checkout_query.equals(update.pre_checkout_query) : update.pre_checkout_query != null)
-            return false;
-        if (poll != null ? !poll.equals(update.poll) : update.poll != null) return false;
-        return poll_answer != null ? poll_answer.equals(update.poll_answer) : update.poll_answer == null;
+        return Objects.equals(update_id, update.update_id) &&
+                Objects.equals(message, update.message) &&
+                Objects.equals(edited_message, update.edited_message) &&
+                Objects.equals(channel_post, update.channel_post) &&
+                Objects.equals(edited_channel_post, update.edited_channel_post) &&
+                Objects.equals(inline_query, update.inline_query) &&
+                Objects.equals(chosen_inline_result, update.chosen_inline_result) &&
+                Objects.equals(callback_query, update.callback_query) &&
+                Objects.equals(shipping_query, update.shipping_query) &&
+                Objects.equals(pre_checkout_query, update.pre_checkout_query) &&
+                Objects.equals(poll, update.poll) &&
+                Objects.equals(poll_answer, update.poll_answer) &&
+                Objects.equals(my_chat_member, update.my_chat_member) &&
+                Objects.equals(chat_member, update.chat_member);
     }
 
     @Override
@@ -119,6 +122,8 @@ public class Update implements Serializable {
                 ", pre_checkout_query=" + pre_checkout_query +
                 ", poll=" + poll +
                 ", poll_answer=" + poll_answer +
+                ", my_chat_member=" + my_chat_member +
+                ", chat_member=" + chat_member +
                 '}';
     }
 }
