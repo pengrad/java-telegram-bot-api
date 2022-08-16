9596bcae7014c0073d0e9008160ad555beb6066a
Mirco Ianese <mirco.ianese@gmail.com>
2022-08-16 18:13:54 +0200

    Added the field sticker_type to the class StickerSet. Field contains_masks deprecated.

--- library/src/main/java/com/pengrad/telegrambot/model/StickerSet.java
+++ library/src/main/java/com/pengrad/telegrambot/model/StickerSet.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
 
+import com.pengrad.telegrambot.model.Sticker.Type;
+
 /**
  * Stas Parshin
  * 23 July 2017
@@ -13,6 +15,7 @@ public class StickerSet implements Serializable {
 
     private String name;
     private String title;
+    private Type sticker_type;
     private Boolean is_animated;
     private Boolean is_video;
     private Boolean contains_masks;
@@ -27,10 +30,18 @@ public class StickerSet implements Serializable {
         return title;
     }
 
+    public Type stickerType() {
+        return sticker_type;
+    }
+
     public Boolean isAnimated() {
         return is_animated;
     }
 
+    /** 
+     *  @deprecated Use type() and check if it equals to Type.mask
+     */   
+    @Deprecated
     public Boolean containsMasks() {
         return contains_masks;
     }
@@ -54,16 +65,16 @@ public class StickerSet implements Serializable {
         StickerSet that = (StickerSet) o;
         return Objects.equals(name, that.name) &&
                 Objects.equals(title, that.title) &&
+                Objects.equals(sticker_type, that.sticker_type) &&
                 Objects.equals(is_animated, that.is_animated) &&
                 Objects.equals(is_video, that.is_video) &&
-                Objects.equals(contains_masks, that.contains_masks) &&
                 Arrays.equals(stickers, that.stickers) &&
                 Objects.equals(thumb, that.thumb);
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hash(name, title, is_animated, is_video, contains_masks, thumb);
+        int result = Objects.hash(name, title, sticker_type, is_animated, is_video, thumb);
         result = 31 * result + Arrays.hashCode(stickers);
         return result;
     }
@@ -73,9 +84,9 @@ public class StickerSet implements Serializable {
         return "StickerSet{" +
                 "name='" + name + '\'' +
                 ", title='" + title + '\'' +
+                ", sticker_type='" + sticker_type + '\'' +
                 ", is_animated=" + is_animated +
                 ", is_video=" + is_video +
-                ", contains_masks=" + contains_masks +
                 ", stickers=" + Arrays.toString(stickers) +
                 ", thumb=" + thumb +
                 '}';
