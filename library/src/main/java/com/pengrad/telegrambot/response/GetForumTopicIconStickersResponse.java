67013c822e0ef68c3c414a5564f25e0224f03a60
Dmitry Baboshin <devbyside@gmail.com>
2022-11-08 02:00:21 +0300

    Response returns List

--- library/src/main/java/com/pengrad/telegrambot/response/GetForumTopicIconStickersResponse.java
+++ library/src/main/java/com/pengrad/telegrambot/response/GetForumTopicIconStickersResponse.java
@@ -2,23 +2,23 @@ package com.pengrad.telegrambot.response;
 
 import com.pengrad.telegrambot.model.Sticker;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class GetForumTopicIconStickersResponse extends BaseResponse {
-    private Sticker[] result;
+    private List<Sticker> result;
 
     GetForumTopicIconStickersResponse() {
 
     }
 
-    public Sticker[] stickers() {
+    public List<Sticker> stickers() {
         return result;
     }
 
     @Override
     public String toString() {
         return "GetForumTopicIconStickersResponse{" +
-                "result=" + Arrays.toString(result) +
+                "result=" + result +
                 '}';
     }
 }
