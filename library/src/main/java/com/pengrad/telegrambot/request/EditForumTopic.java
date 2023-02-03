4bc917d9fc144ef9f664d89ba91ff72737e64208
Mirco Ianese <mirco.ianese@gmail.com>
2023-02-03 21:51:18 +0100

    The parameters name and icon_custom_emoji_id of the method editForumTopic are now optional

--- library/src/main/java/com/pengrad/telegrambot/request/EditForumTopic.java
+++ library/src/main/java/com/pengrad/telegrambot/request/EditForumTopic.java
@@ -3,6 +3,21 @@ package com.pengrad.telegrambot.request;
 import com.pengrad.telegrambot.response.BaseResponse;
 
 public class EditForumTopic extends BaseRequest<EditForumTopic, BaseResponse> {
+
+    public EditForumTopic(Integer chatId,
+                          Integer messageThreadId) {
+        super(BaseResponse.class);
+        add("chat_id", chatId);
+        add("message_thread_id", messageThreadId);
+    }
+
+    public EditForumTopic(String chatId,
+                          Integer messageThreadId) {
+        super(BaseResponse.class);
+        add("chat_id", chatId);
+        add("message_thread_id", messageThreadId);
+    }
+
     public EditForumTopic(Integer chatId,
                           Integer messageThreadId,
                           String name,
@@ -24,4 +39,16 @@ public class EditForumTopic extends BaseRequest<EditForumTopic, BaseResponse> {
         add("name", name);
         add("icon_custom_emoji_id", iconCustomEmojiId);
     }
+
+
+    public EditForumTopic name(String name) {
+        add("name", name);
+        return this;
+    }
+
+    public EditForumTopic iconCustomEmojiId(String icon_custom_emoji_id) {
+        add("icon_custom_emoji_id", icon_custom_emoji_id);
+        return this;
+    }
+
 }
