494aa4c807ebfeaf96b1bd5e60df2319154de848
Mirco Ianese <mirco.ianese@gmail.com>
2023-03-26 15:02:30 +0200

    replacing the parameter png_sticker in the method uploadStickerFile with the parameters sticker and sticker_format

--- library/src/main/java/com/pengrad/telegrambot/request/UploadStickerFile.java
+++ library/src/main/java/com/pengrad/telegrambot/request/UploadStickerFile.java
@@ -1,5 +1,7 @@
 package com.pengrad.telegrambot.request;
 
+import com.pengrad.telegrambot.model.Sticker.Format;
+import com.pengrad.telegrambot.model.request.InputFile;
 import com.pengrad.telegrambot.response.GetFileResponse;
 
 /**
@@ -8,8 +10,23 @@ import com.pengrad.telegrambot.response.GetFileResponse;
  */
 public class UploadStickerFile extends AbstractUploadRequest<UploadStickerFile, GetFileResponse> {
 
+    /**
+     * @deprecated Use constructor with the InputFile type (since API v6.6)
+     */
+    @Deprecated
     public UploadStickerFile(Long userId, Object pngSticker) {
         super(GetFileResponse.class, "png_sticker", pngSticker);
         add("user_id", userId);
     }
+
+    /**
+     * @param stickerFormat must be one of “static”, “animated”, “video”.
+     */
+    public UploadStickerFile(Long userId, InputFile sticker, Format stickerFormat) {
+        super(GetFileResponse.class, "sticker", sticker);
+        add("user_id", userId);
+        add("sticker_format", stickerFormat.name());
+    }
+
+
 }
