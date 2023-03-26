d7051d1affd063ce75d6f9bf64fe15d370070c22
Mirco Ianese <mirco.ianese@gmail.com>
2023-03-26 15:20:05 +0200

    Added the method setStickerMaskPosition

---
+++ library/src/main/java/com/pengrad/telegrambot/request/SetStickerMaskPosition.java
@@ -0,0 +1,28 @@
+package com.pengrad.telegrambot.request;
+
+import com.pengrad.telegrambot.model.MaskPosition;
+import com.pengrad.telegrambot.response.BaseResponse;
+
+
+public class SetStickerMaskPosition extends BaseRequest<SetStickerMaskPosition, BaseResponse> {
+
+    /**
+     * 
+     * @param sticker File identifier of the sticker
+     */
+    public SetStickerMaskPosition(String sticker) {
+        super(BaseResponse.class);
+        add("sticker", sticker);
+    }
+
+    /**
+     * 
+     * @param maskPosition Where the mask should be placed on faces. Omit the parameter to remove the mask position.
+     * @return
+     */
+    public SetStickerMaskPosition keywords(MaskPosition maskPosition) {
+        add("mask_position", maskPosition);
+        return this;
+    }
+
+}
