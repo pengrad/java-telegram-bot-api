9cd21c5856e62ede2d60effe8c882db33e2a41ec
Mirco Ianese <mirco.ianese@gmail.com>
2023-04-22 10:37:51 +0200

    Added the ability to set different bot names for different user languages using the method setMyName

---
+++ library/src/main/java/com/pengrad/telegrambot/request/SetMyName.java
@@ -0,0 +1,31 @@
+package com.pengrad.telegrambot.request;
+
+import com.pengrad.telegrambot.response.BaseResponse;
+
+public class SetMyName extends BaseRequest<SetMyName, BaseResponse> {
+
+    public SetMyName() {
+        super(BaseResponse.class);
+    }
+    
+    /**
+     * 
+     * @param name New bot name; 0-64 characters. Pass an empty string to remove the dedicated name for the given language.
+     * @return
+     */
+    public SetMyName name(String name) {
+        add("name", name);
+        return this;
+    }
+
+    /**
+     * 
+     * @param languageCode A two-letter ISO 639-1 language code. If empty, the name will be shown to all users for whose language there is no dedicated name.
+     * @return
+     */
+    public SetMyName languageCode(String languageCode) {
+        add("language_code", languageCode);
+        return this;
+    }
+
+}
