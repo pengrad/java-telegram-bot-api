fa969305e07446bd63eb5a8b704c8e67a43e200e
Mirco Ianese <mirco.ianese@gmail.com>
2023-08-22 19:10:42 +0200

    Added the field voter_chat to the class PollAnswer

--- library/src/main/java/com/pengrad/telegrambot/model/PollAnswer.java
+++ library/src/main/java/com/pengrad/telegrambot/model/PollAnswer.java
@@ -11,11 +11,16 @@ public class PollAnswer implements Serializable {
     private final static long serialVersionUID = 0L;
 
     private String poll_id;
+    private Chat voter_chat;
     private User user;
     private Integer[] option_ids;
 
     public String pollId() {
         return poll_id;
+    }
+
+    public Chat voterChat() {
+        return voter_chat;
     }
 
     public User user() {
@@ -34,6 +39,7 @@ public class PollAnswer implements Serializable {
         PollAnswer that = (PollAnswer) o;
 
         if (poll_id != null ? !poll_id.equals(that.poll_id) : that.poll_id != null) return false;
+        if (voter_chat != null ? !voter_chat.equals(that.voter_chat) : that.voter_chat != null) return false;
         if (user != null ? !user.equals(that.user) : that.user != null) return false;
         // Probably incorrect - comparing Object[] arrays with Arrays.equals
         return Arrays.equals(option_ids, that.option_ids);
@@ -42,6 +48,7 @@ public class PollAnswer implements Serializable {
     @Override
     public int hashCode() {
         int result = poll_id != null ? poll_id.hashCode() : 0;
+        result = 31 * result + (voter_chat != null ? voter_chat.hashCode() : 0);
         result = 31 * result + (user != null ? user.hashCode() : 0);
         result = 31 * result + Arrays.hashCode(option_ids);
         return result;
@@ -51,6 +58,7 @@ public class PollAnswer implements Serializable {
     public String toString() {
         return "PollAnswer{" +
                 "poll_id='" + poll_id + '\'' +
+                ", voter_chat=" + voter_chat +
                 ", user=" + user +
                 ", option_ids=" + Arrays.toString(option_ids) +
                 '}';
