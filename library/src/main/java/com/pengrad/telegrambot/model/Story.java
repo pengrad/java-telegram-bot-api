74f8483d08a0c113648b0fee4ddeb520c37d4f71
Andrew Anfanik <me@anfanik.me>
2024-02-16 15:36:57 +0300

    Add chat and id fields to Story

--- library/src/main/java/com/pengrad/telegrambot/model/Story.java
+++ library/src/main/java/com/pengrad/telegrambot/model/Story.java
@@ -1,35 +1,47 @@
 package com.pengrad.telegrambot.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * 
  * 18/08/2023.
  */
 
-public class Story implements Serializable {    
+public class Story implements Serializable {
+
     private final static long serialVersionUID = 0L;
 
+    private Chat chat;
+    private Integer id;
+
+    public Chat chat() {
+        return chat;
+    }
+
+    public Integer id() {
+        return id;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Story story = (Story) o;
-        // todo: fix when fields are added
-        return true;
+        return Objects.equals(chat, story.chat)
+            && Objects.equals(id, story.id);
     }
 
     @Override
     public int hashCode() {
-        // todo: fix when fields are added
-        return 0;
+        return Objects.hash(chat, id);
     }
 
     @Override
     public String toString() {
-        // todo: fix when fields are added
-        return "Story{" + '}';
+        return "Story{" +
+            "chat=" + chat +
+            ", id=" + id +
+            '}';
     }
-
 }
