38cdd3568283ab1ee820410d9df6c1019528273c
Andrew Anfanik <me@anfanik.me>
2024-04-06 01:52:56 +0300

    Add SharedUser type and add users field to UsersShared

--- library/src/main/java/com/pengrad/telegrambot/model/UsersShared.java
+++ library/src/main/java/com/pengrad/telegrambot/model/UsersShared.java
@@ -1,5 +1,7 @@
 package com.pengrad.telegrambot.model;
 
+import com.pengrad.telegrambot.model.shared.SharedUser;
+
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
@@ -9,11 +11,20 @@ public class UsersShared implements Serializable {
 
     private Integer request_id;
     private Integer[] user_ids;
+    private SharedUser[] users;
 
     public Integer requestId() {
         return request_id;
     }
 
+    public SharedUser[] users() {
+        return users;
+    }
+
+    /**
+     * @deprecated Use {@link UsersShared#users()} instead
+     */
+    @Deprecated
     public Integer[] userIds() {
         return user_ids;
     }
@@ -21,23 +32,27 @@ public class UsersShared implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof UsersShared)) return false;
         UsersShared that = (UsersShared) o;
-        return Objects.equals(request_id, that.request_id) && Arrays.equals(user_ids, that.user_ids);
+        return Objects.equals(request_id, that.request_id)
+            && Arrays.equals(user_ids, that.user_ids)
+            && Arrays.equals(users, that.users);
     }
 
     @Override
     public int hashCode() {
         int result = Objects.hash(request_id);
         result = 31 * result + Arrays.hashCode(user_ids);
+        result = 31 * result + Arrays.hashCode(users);
         return result;
     }
 
     @Override
     public String toString() {
         return "UsersShared{" +
-                "request_id='" + request_id + '\'' +
-                ", user_ids='" + Arrays.toString(user_ids) + '\'' +
-                '}';
+            "request_id=" + request_id +
+            ", user_ids=" + Arrays.toString(user_ids) +
+            ", users=" + Arrays.toString(users) +
+            '}';
     }
 }
