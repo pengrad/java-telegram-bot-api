12697dcaffbfb27d1643db5814047c10748d34b0
Andrew Anfanik <me@anfanik.me>
2024-04-06 01:56:29 +0300

    Add fields to ChatShared: title, username, photo

--- library/src/main/java/com/pengrad/telegrambot/model/ChatShared.java
+++ library/src/main/java/com/pengrad/telegrambot/model/ChatShared.java
@@ -1,6 +1,7 @@
 package com.pengrad.telegrambot.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class ChatShared implements Serializable {
@@ -8,6 +9,9 @@ public class ChatShared implements Serializable {
 
     private Integer request_id;
     private Long chat_id;
+    private String title;
+    private String username;
+    private PhotoSize[] photo;
 
     public Integer requestId() {
         return request_id;
@@ -17,6 +21,18 @@ public class ChatShared implements Serializable {
         return chat_id;
     }
 
+    public String title() {
+        return title;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    public PhotoSize[] photo() {
+        return photo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -24,21 +40,30 @@ public class ChatShared implements Serializable {
 
         ChatShared that = (ChatShared) o;
 
-        return Objects.equals(request_id, that.request_id) &&
-                Objects.equals(chat_id, that.chat_id);
+        return Objects.equals(request_id, that.request_id)
+            && Objects.equals(chat_id, that.chat_id)
+            && Objects.equals(title, that.title)
+            && Objects.equals(username, that.username)
+            && Arrays.equals(photo, that.photo);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(request_id,
-            chat_id);
+            chat_id,
+            title,
+            username,
+            Arrays.hashCode(photo));
     }
 
     @Override
     public String toString() {
         return "ChatShared{" +
-                "request_id='" + request_id + '\'' +
-                ", chat_id='" + chat_id + '\'' +
-                '}';
+            "request_id=" + request_id +
+            ", chat_id=" + chat_id +
+            ", title='" + title + '\'' +
+            ", username='" + username + '\'' +
+            ", photo=" + Arrays.toString(photo) +
+            '}';
     }
 }
