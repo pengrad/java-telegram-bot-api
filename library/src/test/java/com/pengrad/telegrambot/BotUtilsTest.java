90590ca6aaa3b2dcda5a1a5601ff09e5faa2629b
Roman Prudkogliad <romanp@ligolab.com>
2024-04-16 14:53:43 +0300

    fix int in shared_users json serialization

--- library/src/test/java/com/pengrad/telegrambot/BotUtilsTest.java
+++ library/src/test/java/com/pengrad/telegrambot/BotUtilsTest.java
@@ -20,11 +20,12 @@ import static org.junit.Assert.assertNull;
 public class BotUtilsTest {
 
     final String updateStr = "{\"update_id\":874199391,\n" +
-            "\"message\":{\"message_id\":33111,\"from\":{\"id\":1231231231,\"is_bot\":false,\"first_name\":\"RRRR\",\"username\":\"RRRR54321\"},\"chat\":{\"id\":-23123123123123,\"title\":\"hhh iiiiii ccccc\",\"type\":\"supergroup\"},\"date\":1579958705,\"text\":\"block the news\"}}";
+            "\"message\":{\"message_id\":33111,\"from\":{\"id\":1231231231,\"is_bot\":false,\"first_name\":\"RRRR\",\"username\":\"RRRR54321\"},\"chat\":{\"id\":-23123123123123,\"title\":\"hhh iiiiii ccccc\",\"type\":\"supergroup\"},\"user_shared\":{\"user_id\":6111111111,\"request_id\":1},\"users_shared\":{\"user_ids\":[6111111111],\"users\":[{\"user_id\":6111111111,\"first_name\":\"FirstNameTest\",\"last_name\":\"LastNameTest\"}],\"request_id\":1},\"date\":1579958705,\"text\":\"block the news\"}}";
 
     private void check(Update update) {
         assertEquals(update.updateId(), Integer.valueOf(874199391));
         assertEquals(update.message().messageId(), Integer.valueOf(33111));
+        assertEquals(update.message().usersShared().users()[0].userId(), Long.valueOf(6111111111L));
     }
 
     @Test
@@ -51,5 +52,4 @@ public class BotUtilsTest {
         assertNull(BotUtils.fromJson(null, SendResponse.class));
         assertNull(BotUtils.fromJson("",SendResponse.class));
     }
-
 }
