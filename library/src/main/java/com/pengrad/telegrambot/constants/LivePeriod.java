63f50380b9ce0907b41c6d20abf2d22eb5e4f5b6
Mirco Ianese <mirco.ianese@gmail.com>
2024-05-29 08:18:45 +0200

    remove constructor for LivePeriod class

--- library/src/main/java/com/pengrad/telegrambot/constants/LivePeriod.java
+++ library/src/main/java/com/pengrad/telegrambot/constants/LivePeriod.java
@@ -3,5 +3,7 @@ package com.pengrad.telegrambot.constants;
 public class LivePeriod {
 
     public final static Integer INFINITE_LIVE_PERIOD = Integer.MAX_VALUE;
+
+    private LivePeriod() { }
     
 }
