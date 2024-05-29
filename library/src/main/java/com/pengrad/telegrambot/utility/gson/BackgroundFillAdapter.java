0f78926cf2fb2ac894f4466f3c8a9fad3cdbc28e
Mirco Ianese <mirco.ianese@gmail.com>
2024-05-29 10:55:49 +0200

    Fix: added BackgroundFillAdapter and BackgroundTypeAdapter for deserialization (api v7.3)

---
+++ library/src/main/java/com/pengrad/telegrambot/utility/gson/BackgroundFillAdapter.java
@@ -0,0 +1,28 @@
+package com.pengrad.telegrambot.utility.gson;
+
+import com.google.gson.*;
+import com.pengrad.telegrambot.model.chatbackground.BackgroundFill;
+import com.pengrad.telegrambot.model.chatbackground.BackgroundFillFreeformGradient;
+import com.pengrad.telegrambot.model.chatbackground.BackgroundFillGradient;
+import com.pengrad.telegrambot.model.chatbackground.BackgroundFillSolid;
+
+import java.lang.reflect.Type;
+
+public class BackgroundFillAdapter implements JsonDeserializer<BackgroundFill> {
+
+    @Override
+    public BackgroundFill deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject object = element.getAsJsonObject();
+        String discriminator = object.getAsJsonPrimitive("type").getAsString();
+
+        if (BackgroundFillSolid.TYPE.equals(discriminator)) {
+            return context.deserialize(object, BackgroundFillSolid.class);
+        } else if (BackgroundFillGradient.TYPE.equals(discriminator)) {
+            return context.deserialize(object, BackgroundFillGradient.class);
+        } else if (BackgroundFillFreeformGradient.TYPE.equals(discriminator)) {
+            return context.deserialize(object, BackgroundFillFreeformGradient.class);
+        }
+
+        return context.deserialize(object, BackgroundFill.class);
+    }
+}
