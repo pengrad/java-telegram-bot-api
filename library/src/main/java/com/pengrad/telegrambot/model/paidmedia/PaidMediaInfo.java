5654454b4d574da3ccff50ee7f3216e26e3dd32d
Mirco Ianese <mirco.ianese@gmail.com>
2024-07-04 15:25:55 +0200

    Added the field paid_media to the classes Message and ExternalReplyInfo

---
+++ library/src/main/java/com/pengrad/telegrambot/model/paidmedia/PaidMediaInfo.java
@@ -0,0 +1,46 @@
+package com.pengrad.telegrambot.model.paidmedia;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class PaidMediaInfo implements Serializable {
+
+    private final static long serialVersionUID = 0L;
+
+    private Integer star_count;
+
+    private PaidMedia[] paid_media;
+
+    public Integer starCount() {
+        return star_count;
+    }
+
+    public PaidMedia[] paidMedia() {
+        return paid_media;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PaidMediaInfo that = (PaidMediaInfo) o;
+        return Objects.equals(star_count, that.star_count) &&
+                    Arrays.equals(paid_media, that.paid_media);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(star_count, paid_media);
+    }
+
+    @Override
+    public String toString() {
+        return "PaidMediaInfo{" +
+                "star_count='" + star_count + "'," +
+                "paid_media='" + Arrays.toString(paid_media) + "'" +
+                '}';
+    }
+
+}
