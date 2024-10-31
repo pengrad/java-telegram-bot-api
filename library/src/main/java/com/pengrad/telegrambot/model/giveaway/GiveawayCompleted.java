138b04b19a76b83988a66bc248bb0ff59b8ec2cb
Andrew Anfanik <me@anfanik.me>
2024-10-31 18:59:09 +0200

    Add is_star_giveaway to GiveawayCompleted

--- library/src/main/java/com/pengrad/telegrambot/model/giveaway/GiveawayCompleted.java
+++ library/src/main/java/com/pengrad/telegrambot/model/giveaway/GiveawayCompleted.java
@@ -12,7 +12,7 @@ public class GiveawayCompleted implements Serializable {
     private Integer winner_count;
     private Integer unclaimed_prize_count;
     private Message giveaway_message;
-
+    private Boolean is_star_giveaway;
 
     public Integer winnerCount() {
         return winner_count;
@@ -26,6 +26,9 @@ public class GiveawayCompleted implements Serializable {
         return giveaway_message;
     }
 
+    public Boolean isStarGiveaway() {
+        return is_star_giveaway;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -34,12 +37,17 @@ public class GiveawayCompleted implements Serializable {
         GiveawayCompleted that = (GiveawayCompleted) o;
         return Objects.equals(winner_count, that.winner_count)
                 && Objects.equals(unclaimed_prize_count, that.unclaimed_prize_count)
-                && Objects.equals(giveaway_message, that.giveaway_message);
+                && Objects.equals(giveaway_message, that.giveaway_message)
+                && Objects.equals(is_star_giveaway, that.is_star_giveaway);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(winner_count, unclaimed_prize_count, giveaway_message);
+        return Objects.hash(
+            winner_count,
+            unclaimed_prize_count,
+            giveaway_message,
+            is_star_giveaway);
     }
 
     @Override
@@ -48,6 +56,7 @@ public class GiveawayCompleted implements Serializable {
                 "winner_count='" + winner_count + "'," +
                 "unclaimed_prize_count='" + unclaimed_prize_count + "'," +
                 "giveaway_message='" + giveaway_message + "'" +
+                "is_star_giveaway='" + is_star_giveaway + "'" +
                 '}';
     }
 
