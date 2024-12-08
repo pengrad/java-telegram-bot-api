03542b8fb00a6fd6e970fbec6057aea82121955d
Andrew Anfanik <me@anfanik.me>
2024-12-08 06:01:29 +0200

    Add affiliate field to TransactionPartnerUser & create AffiliateInfo

--- library/src/main/java/com/pengrad/telegrambot/model/stars/partner/TransactionPartnerUser.kt
+++ library/src/main/java/com/pengrad/telegrambot/model/stars/partner/TransactionPartnerUser.kt
@@ -3,10 +3,12 @@ package com.pengrad.telegrambot.model.stars.partner
 import com.pengrad.telegrambot.model.User
 import com.pengrad.telegrambot.model.gift.Gift
 import com.pengrad.telegrambot.model.paidmedia.PaidMedia
+import com.pengrad.telegrambot.model.stars.affiliate.AffiliateInfo
 import com.pengrad.telegrambot.model.stars.partner.TransactionPartnerType.USER
 
 data class TransactionPartnerUser(
     @get:JvmName("user") val user: User,
+    @get:JvmName("affiliate") val affiliate: AffiliateInfo,
     @get:JvmName("invoicePayload") val invoicePayload: String? = null,
     @get:JvmName("paidMedia") val paidMedia: Array<PaidMedia>? = null,
     @get:JvmName("paidMediaPayload") val paidMediaPayload: String? = null,
@@ -25,6 +27,7 @@ data class TransactionPartnerUser(
 
         if (subscriptionPeriod != other.subscriptionPeriod) return false
         if (user != other.user) return false
+        if (affiliate != other.affiliate) return false
         if (invoicePayload != other.invoicePayload) return false
         if (paidMedia != null) {
             if (other.paidMedia == null) return false
@@ -40,6 +43,7 @@ data class TransactionPartnerUser(
     override fun hashCode(): Int {
         var result = subscriptionPeriod ?: 0
         result = 31 * result + user.hashCode()
+        result = 31 * result + affiliate.hashCode()
         result = 31 * result + (invoicePayload?.hashCode() ?: 0)
         result = 31 * result + (paidMedia?.contentHashCode() ?: 0)
         result = 31 * result + (paidMediaPayload?.hashCode() ?: 0)
