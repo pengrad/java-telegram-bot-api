3ff0c2d02d29a87f2bea5f035f34d35fe184f5d6
Mirco Ianese <mirco.ianese@gmail.com>
2025-08-19 18:10:12 +0200

    Added the class Checklist representing a checklist.

---
+++ library/src/main/java/com/pengrad/telegrambot/model/checklist/Checklist.kt
@@ -0,0 +1,36 @@
+package com.pengrad.telegrambot.model.checklist
+
+import com.pengrad.telegrambot.model.MessageEntity
+import com.pengrad.telegrambot.model.User
+import com.pengrad.telegrambot.model.gift.GiftInfo
+
+data class Checklist(
+
+    @get:JvmName("title") val title: String,
+    @get:JvmName("titleEntities") var titleEntities: Array<MessageEntity>?,
+    @get:JvmName("tasks") val tasks: Array<ChecklistTask>,
+    @get:JvmName("othersCanAddTasks") val othersCanAddTasks: Boolean?,
+    @get:JvmName("othersCanAddTasks") val othersCanMarkTasksAsDone: Boolean?
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Checklist) return false
+
+        return title == other.title &&
+                titleEntities contentEquals other.titleEntities &&
+                tasks contentEquals other.tasks &&
+                othersCanAddTasks == other.othersCanAddTasks &&
+                othersCanMarkTasksAsDone == other.othersCanMarkTasksAsDone
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + (titleEntities?.contentHashCode() ?: 0)
+        result = 31 * result + (tasks?.contentHashCode() ?: 0)
+        result = 31 * result + (othersCanAddTasks?.hashCode() ?: 0)
+        result = 31 * result + (othersCanMarkTasksAsDone?.hashCode() ?: 0)
+        return result
+    }
+
+}
