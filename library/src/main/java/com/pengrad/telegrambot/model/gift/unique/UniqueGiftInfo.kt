70462f8de3c58c0edf49937f5dca70f839206f6b
Mirco Ianese <mirco.ianese@gmail.com>
2025-08-19 18:47:33 +0200

    Added the field next_transfer_date to the classes OwnedGiftUnique and UniqueGiftInfo.

--- library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGiftInfo.kt
+++ library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGiftInfo.kt
@@ -5,5 +5,6 @@ data class UniqueGiftInfo(
     @get:JvmName("gift")  val gift: UniqueGift,
     @get:JvmName("origin")  val origin: String,
     @get:JvmName("ownedGiftId")  val ownedGiftId: String?,
-    @get:JvmName("transferStarCount")  val transferStarCount: Int?
+    @get:JvmName("transferStarCount")  val transferStarCount: Int?,
+    @get:JvmName("nextTransferDate") var nextTransferDate: Long?
 )
