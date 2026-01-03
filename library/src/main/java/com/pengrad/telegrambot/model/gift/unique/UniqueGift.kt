2d9961a52f61cebdd1a955da87cfd6c93996e0fd
Mirco Ianese <mirco.ianese@gmail.com>
2026-01-03 15:22:18 +0100

    Added the field colors to the class UniqueGift

--- library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGift.kt
+++ library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGift.kt
@@ -13,5 +13,6 @@ data class UniqueGift(
     @get:JvmName("publisherChat") val publisherChat: Chat? = null,
     @get:JvmName("giftId") val giftId: String? = null,
     @get:JvmName("isFromBlockchain") val isFromBlockchain: Boolean? = null,
-    @get:JvmName("isPremium") val isPremium: Boolean? = null
+    @get:JvmName("isPremium") val isPremium: Boolean? = null,
+    @get:JvmName("colors") val colors: UniqueGiftColors? = null
 )
