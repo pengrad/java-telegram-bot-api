c253b8f5064899b34aff046ffaf1f3df44ef8835
Mirco Ianese <mirco.ianese@gmail.com>
2026-08-25 21:24:04 +0200

    Added the fields text, entities and is_private to the class UniqueGiftInfo

--- library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGiftInfo.kt
+++ library/src/main/java/com/pengrad/telegrambot/model/gift/unique/UniqueGiftInfo.kt
@@ -1,5 +1,7 @@
 package com.pengrad.telegrambot.model.gift.unique
 
+import com.pengrad.telegrambot.model.MessageEntity
+
 @Suppress("unused")
 data class UniqueGiftInfo(
     @get:JvmName("gift")  val gift: UniqueGift,
@@ -10,5 +12,53 @@ data class UniqueGiftInfo(
     @Deprecated("Use lastResaleCurrency and lastResaleAmount instead")
     @get:JvmName("lastResaleStarCount")  val lastResaleStarCount: Int?,
     @get:JvmName("lastResaleCurrency")  val lastResaleCurrency: String?,
-    @get:JvmName("lastResaleAmount")  val lastResaleAmount: Long?
-)
+    @get:JvmName("lastResaleAmount")  val lastResaleAmount: Long?,
+    @get:JvmName("text") val text: String? = null,
+    @get:JvmName("entities") val entities: Array<MessageEntity>? = null,
+    @get:JvmName("isPrivate") val isPrivate: Boolean? = null,
+) {
+
+    @Suppress("DEPRECATION")
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is UniqueGiftInfo) return false
+
+        return gift == other.gift &&
+                origin == other.origin &&
+                ownedGiftId == other.ownedGiftId &&
+                transferStarCount == other.transferStarCount &&
+                nextTransferDate == other.nextTransferDate &&
+                lastResaleStarCount == other.lastResaleStarCount &&
+                lastResaleCurrency == other.lastResaleCurrency &&
+                lastResaleAmount == other.lastResaleAmount &&
+                text == other.text &&
+                entities contentEquals other.entities &&
+                isPrivate == other.isPrivate
+    }
+
+    @Suppress("DEPRECATION")
+    override fun hashCode(): Int {
+        var result = gift.hashCode()
+        result = 31 * result + origin.hashCode()
+        result = 31 * result + (ownedGiftId?.hashCode() ?: 0)
+        result = 31 * result + (transferStarCount ?: 0)
+        result = 31 * result + (nextTransferDate?.hashCode() ?: 0)
+        result = 31 * result + (lastResaleStarCount ?: 0)
+        result = 31 * result + (lastResaleCurrency?.hashCode() ?: 0)
+        result = 31 * result + (lastResaleAmount?.hashCode() ?: 0)
+        result = 31 * result + (text?.hashCode() ?: 0)
+        result = 31 * result + (entities?.contentHashCode() ?: 0)
+        result = 31 * result + (isPrivate?.hashCode() ?: 0)
+        return result
+    }
+
+    @Suppress("DEPRECATION")
+    override fun toString(): String {
+        return "UniqueGiftInfo(gift=$gift, origin=$origin, ownedGiftId=$ownedGiftId, " +
+                "transferStarCount=$transferStarCount, nextTransferDate=$nextTransferDate, " +
+                "lastResaleStarCount=$lastResaleStarCount, lastResaleCurrency=$lastResaleCurrency, " +
+                "lastResaleAmount=$lastResaleAmount, text=$text, " +
+                "entities=${entities?.contentToString()}, isPrivate=$isPrivate)"
+    }
+
+}
